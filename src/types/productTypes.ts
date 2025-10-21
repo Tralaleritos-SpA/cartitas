94e69e923bf1b2cc03bf265c44c49c0aba98108f
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-20 22:30:49 -0300

    api interface types

---
+++ src/types/productTypes.ts
@@ -0,0 +1,28 @@
+export interface Brand {
+    id: string;
+    isActive: boolean;
+    name: string;
+}
+
+export interface Category {
+    id: string;
+    active: boolean;
+    name: string;
+}
+
+export interface Product {
+    id: string;
+    isActive?: boolean;
+    name: string;
+    brand: Brand;
+    category: Category;
+    stock?: number;
+    price: number;
+
+    // Optional fields
+    img_url: string | "";
+    description: string | null;
+    quantity?: number;
+    min_player_number?: number;
+    max_player_number?: number;
+}
