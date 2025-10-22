900f3f03c36c59baa1b67d0abaf550becf290405
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-22 00:10:52 -0300

    add fetch active and fix error throwing

--- src/services/brandService.ts
+++ src/services/brandService.ts
@@ -1,8 +1,8 @@
 import type { Brand } from "../types/productTypes";
 
-export async function fetchBrands(): Promise<Brand[]> {
-    const apiURL = "http://localhost:6969/api/v1/brands/active";
+const apiURL = "http://localhost:6969/api/v1/brands";
 
+export async function fetchBrands(): Promise<Brand[]> {
     try {
         const response = await fetch(apiURL);
 
@@ -14,6 +14,26 @@ export async function fetchBrands(): Promise<Brand[]> {
 
         return brands;
     } catch (error) {
-        throw "API Error in brandService: " + error;
+        const errorMessage =
+            error instanceof Error ? error.message : String(error);
+        throw new Error("API Error in productService:" + errorMessage);
+    }
+}
+
+export async function fetchActiveBrands(): Promise<Brand[]> {
+    try {
+        const response = await fetch(apiURL + "/active");
+
+        if (!response.ok) {
+            throw `Failed to fetch active brands. Status: ${response.status}`;
+        }
+
+        const brands: Brand[] = await response.json();
+
+        return brands;
+    } catch (error) {
+        const errorMessage =
+            error instanceof Error ? error.message : String(error);
+        throw new Error("API Error in productService:" + errorMessage);
     }
 }
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,8 +1,8 @@
 import type { Product } from "../types/productTypes";
 
-export async function fetchProducts(): Promise<Product[]> {
-    const apiURL = "http://localhost:6969/api/v1/products/active";
+const apiURL = "http://localhost:6969/api/v1/products";
 
+export async function fetchProducts(): Promise<Product[]> {
     try {
         const response = await fetch(apiURL);
 
@@ -14,6 +14,26 @@ export async function fetchProducts(): Promise<Product[]> {
 
         return products;
     } catch (error) {
-        throw "API Error in productService:" + error;
+        const errorMessage =
+            error instanceof Error ? error.message : String(error);
+        throw new Error("API Error in productService:" + errorMessage);
+    }
+}
+
+export async function fetchActiveProducts(): Promise<Product[]> {
+    try {
+        const response = await fetch(apiURL + "/active");
+
+        if (!response.ok) {
+            throw `Failed to fetch active products. Status ${response.status}`;
+        }
+
+        const activeProducts: Product[] = await response.json();
+
+        return activeProducts;
+    } catch (error) {
+        const errorMessage =
+            error instanceof Error ? error.message : String(error);
+        throw new Error("API Error in productService:" + errorMessage);
     }
 }
