942170a4e3aa3b492f6f73badffbdfd868401ea5
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-22 01:43:54 -0300

    testing page

--- src/pages/AddItemsTestApi.tsx
+++ src/pages/AddItemsTestApi.tsx
@@ -1,34 +1,54 @@
+import { Form } from "react-bootstrap";
 import { useFetch } from "../hooks/useFetch";
 import { fetchActiveBrands } from "../services/brandService";
+import BrandCreationForm from "../components/BrandCreationForm";
+import CategoryCreationForm from "../components/CategoryCreationForm";
+import { fetchActiveCategories } from "../services/categoryService";
 
 function AddItemsTestApi() {
-    const { data, loading, error } = useFetch(fetchActiveBrands);
+    const {
+        data: dataBrand,
+        loading: loadingBrand,
+        error: errorBrand,
+    } = useFetch(fetchActiveBrands);
 
-    if (loading) {
+    const {
+        data: dataCategory,
+        loading: loadingCategory,
+        error: errorCategory,
+    } = useFetch(fetchActiveCategories);
+
+    if (loadingBrand || loadingCategory) {
         return (
             <div className="container">
-                <p>Loading products...</p>
+                <p>Loading...</p>
             </div>
         );
     }
 
-    if (error) {
-        return (
-            <div className="container alert alert-danger">
-                Error: {error.toString()}
-            </div>
-        );
+    if (errorBrand || errorCategory) {
+        return <div className="container alert alert-danger">Error: g</div>;
     }
 
     return (
         <div className="container">
-            <h1>brands active</h1>
-            {data?.map((brand, index) => (
-                <div key={index}>
-                    <p>id: {brand.id}</p>
-                    <p>{brand.name}</p>
-                </div>
-            ))}
+            <div className="py-3">
+                <Form.Select>
+                    {dataBrand?.map((brand) => (
+                        <option key={brand.id}>{brand.name}</option>
+                    ))}
+                </Form.Select>
+                <BrandCreationForm />
+            </div>
+            <div className="py-3">
+                <Form.Select>
+                    {dataCategory?.map((category) => (
+                        <option key={category.id}>{category.name}</option>
+                    ))}
+                </Form.Select>
+
+                <CategoryCreationForm />
+            </div>
         </div>
     );
 }
