0361f8b15428aec7258642f54b1fcee00791cc80
jose <josealvarado.valdes98@gmail.com>
2025-11-07 00:20:35 -0300

    pruebas unitarias del  login completas

--- tests/pages/Login.test.tsx
+++ tests/pages/Login.test.tsx
@@ -1,17 +1,54 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+
+import { use } from "react";
 import users from "../../src/data/user.json"
-import React from "react";
-import { describe } from "vitest";
+import ValidateLogin from "../../src/hooks/Login";
+import { describe, expect, test } from "vitest";
 
 describe('Prueba pagina login', () => {
 
-  const user = users[0]
+  test('debe devolver correcto si el usuario y contraseña  coinciden', () => {
 
-  const email = user.email
-  const password = user.password
+    const user = users[0]
+
+    const email = user.email
+    const password = user.password
+
+    const resultado=ValidateLogin(email,password)
+
+    expect(resultado).toEqual(user)
 
 
+  })
+
+  test('debe devolver false si  el email no coincide', () => {
+    const user= users[1]
+    const password=user.password
+    
+    const resultado =ValidateLogin("elenor@gmail.com", password)
+    
+    expect(resultado).toBe(false)
+  })
+
+  test('debe devolver false si  la contraseña no coincide', () => {
+    const user= users[2]
+    const email=user.email
+    
+    const resultado =ValidateLogin(email, "wrongpassword")
+    
+    expect(resultado).toBe(false)
+  })
+
+  test("devolvera false si el email existe pero la contraseña no", () => {
+    const user=users[2]
+    const email = user.email; 
+    const password = "clave_incorrecta"; // <---- contraseña que no existe en nuestro json pero el email si, no coinciden 
+
+    const resultado = ValidateLogin(email, password);
+    
+    expect(resultado).toBe(false)
+  })
+
+    
+ 
 
 })
-
