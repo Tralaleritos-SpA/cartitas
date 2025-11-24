e0626f8db05183c615ef2c38e6a0581fc6ed18b8
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-11-24 11:45:15 -0300

    testing user

--- src/pages/ApiTest.tsx
+++ src/pages/ApiTest.tsx
@@ -1,9 +1,11 @@
 import RoleCreationForm from "../components/RoleCreationForm";
+import UserCreationForm from "../components/UserCreationForm";
 
 function APITest() {
     return (
         <div className="container">
             <RoleCreationForm />
+            <UserCreationForm />
         </div>
     );
 }
