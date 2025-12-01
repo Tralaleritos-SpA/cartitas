3b8ae49387c04bee11a8191e4bde523116fe4a13
jose <josealvarado.valdes98@gmail.com>
2025-11-30 23:09:05 -0300

    TESTS

--- tests/hooks/Rvalidation.test.ts
+++ tests/hooks/Rvalidation.test.ts
@@ -3,27 +3,27 @@ import { ValidacionR } from "../../src/hooks/ValidacionR";
 
 describe("Validaciones de registro", () => {
   test("si el correo no contiene el @ manda error", () => {
-    const result = ValidacionR("correo", "123456", "123456");
+    const result = ValidacionR("correo", "123456", "123456", "Juan", "Perez");
     expect(result.email).toBe("Ingresa un correo válido.");
   });
 
   test("si la contraseña esta vacia manda error", () => {
-    const result = ValidacionR("correo@valido.com", "", "");
+    const result = ValidacionR("correo@valido.com", "", "", "Juan", "Perez");
     expect(result.password).toBe("Ingresa tu contraseña.");
   });
 
   test("si la contraseña no coincide retorna el error", () => {
-    const result = ValidacionR("correo@valido.com", "123456", "654321");
+    const result = ValidacionR("correo@valido.com", "123456", "654321", "Juan", "Perez");
     expect(result.rpassword).toBe("Las contraseñas no coinciden.");
   });
 
   test(" si todo es válido no retorna nada ", () => {
-    const result = ValidacionR("correo@valido.com", "123456", "123456");
+    const result = ValidacionR("correo@valido.com", "123456", "123456", "Juan", "Perez");
     expect(result).toEqual({});
   });
 
   test("si la contraseña es menor a 6 caracteres manda error", () => {
-    const result = ValidacionR("correomayor6@gmail.com", "123", "123");
+    const result = ValidacionR("correomayor6@gmail.com", "123", "123", "Juan", "Perez");
     expect(result.password).toBe("La contraseña debe tener al menos 6 caracteres.");
   });
 });
