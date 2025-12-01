90803a3f777da9630d99b4f60b5becba4569b4ee
jose <josealvarado.valdes98@gmail.com>
2025-11-30 22:46:01 -0300

    linkear a lista de usuarios

--- src/pages/AdminUsuarios.tsx
+++ src/pages/AdminUsuarios.tsx
@@ -1,4 +1,5 @@
 import UserCreationForm from "../components/UserCreationForm";
+import { Link } from "react-router-dom";
 
 function AdminUsuarios() {
     return (
@@ -6,6 +7,12 @@ function AdminUsuarios() {
             <h2>Usuarios</h2>
             <div className="box">
                 <UserCreationForm />
+            </div>
+
+            <div className="mt-3">
+                <h3>Usuarios</h3>
+                <p>Para ver o editar usuarios existentes, visita la lista:</p>
+                <Link to="/admin/usuarios/existentes" className="button button-primary">Ver usuarios existentes</Link>
             </div>
         </div>
     );
